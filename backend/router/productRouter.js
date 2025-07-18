c44760b46484f99c75b258b9cb802c570930bc53
Nishant bhatia <developernishant0708@gmail.com>
2025-07-18 11:50:45 +0530

    restore deleted by mistake

---
+++ backend/router/productRouter.js
@@ -0,0 +1,84 @@
+const express = require("express")
+const router = express.Router()
+
+const product = [
+    {
+        id: 1,
+        title: "samsung galaxy 8six",
+        price: 2000,
+        description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        quantity: 1,
+        image: "https://i0.wp.com/mikeyarce.com/wp-content/uploads/2021/09/woocommerce-placeholder.png?ssl=1",
+    },
+    {
+        id: 2,
+        title: "vivo 194 v8",
+        price: 1000,
+        description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        quantity: 1,
+        image: "https://i0.wp.com/mikeyarce.com/wp-content/uploads/2021/09/woocommerce-placeholder.png?ssl=1",
+    },
+    {
+        id: 3,
+        title: "I phone 13",
+        price: 4000,
+        description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        quantity: 1,
+        image: "https://i0.wp.com/mikeyarce.com/wp-content/uploads/2021/09/woocommerce-placeholder.png?ssl=1",
+    },
+    {
+        id: 4,
+        title: "oppo 9",
+        price: 4000,
+        description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        quantity: 1,
+        image: "https://i0.wp.com/mikeyarce.com/wp-content/uploads/2021/09/woocommerce-placeholder.png?ssl=1",
+    },
+    {
+        id: 5,
+        title: "Nothing phone 1",
+        price: 4000,
+        description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        quantity: 1,
+        image: "https://i0.wp.com/mikeyarce.com/wp-content/uploads/2021/09/woocommerce-placeholder.png?ssl=1",
+    },
+    {
+        id: 6,
+        title: "One plus nord ce 3",
+        price: 4000,
+        description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        quantity: 1,
+        image: "https://i0.wp.com/mikeyarce.com/wp-content/uploads/2021/09/woocommerce-placeholder.png?ssl=1",
+    },
+    {
+        id: 7,
+        title: "nothing phone 2",
+        price: 4000,
+        description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        quantity: 1,
+        image: "https://i0.wp.com/mikeyarce.com/wp-content/uploads/2021/09/woocommerce-placeholder.png?ssl=1",
+      },
+      {
+        id: 8,
+        title: "google pixele 9",
+        price: 4000,
+        description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        quantity: 1,
+        image: "https://i0.wp.com/mikeyarce.com/wp-content/uploads/2021/09/woocommerce-placeholder.png?ssl=1",
+      }
+]
+
+// router.get("/", (req, res)=>{
+//     res.json(product)
+// })
+
+router.get("/product", (req, res)=>{
+    if (req.url === "/product") {
+        res.render("cards.ejs", {products: product})
+    } else {
+        res.end("Somthing went wrong error: 404")
+    }
+})
+
+
+module.exports = router;
